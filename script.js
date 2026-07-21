d608f5d7bcac87c6e578191d670c1aac4da24952
D4mizr <d4mizr@gmail.com>
2026-07-21 12:17:53 +0200

    Cambio de planes

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("mc-btn").addEventListener("click", () => {
-    window.location.href = "./mc/index.mc.html";
-});
-
-document.getElementById("raspi-btn").addEventListener("click", () => {
-    window.location.href = "./raspi/index.raspi.html";
-});
--- scripts/ip.js
+++
@@ -1,8 +0,0 @@
-async function actualizar() {
-    ethres = await fetch("/network");
-    const ethdatos = await ethres.json();
-    document.getElementById("ip").textContent = "ID: " + ethdatos.ip;
-}
-
-actualizar();
-setInterval(actualizar, 3000);
